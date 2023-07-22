79d61be73cca754396d65016ae830501f4e29f53
jdaeL <joedaniel0804@gmail.com>
2023-07-22 15:38:48 -0500

    app config

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'invetoryApp'
+    'invetoryApp.apps.InventoryappConfig'
 ]
 
 MIDDLEWARE = [
