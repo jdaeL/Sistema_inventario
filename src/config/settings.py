8cd1989c70b94c24a72839e75456d3bdfe6b9602
jdaeL <joedaniel0804@gmail.com>
2023-07-22 16:05:23 -0500

    rest_framework

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventoryApp'
+    'rest_framework',               # con esto funciona el djangorestframework
+    'inventoryApp',
 ]
 
 MIDDLEWARE = [
