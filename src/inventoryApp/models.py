90cf36c1bd031ae5295f34d6a330be21ec4406f6
jdaeL <joedaniel0804@gmail.com>
2023-07-22 16:03:40 -0500

    models

--- src/inventoryApp/models.py
+++ src/inventoryApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    # cod_autor = models.AutoField(primary_key=True)        
+    nombre = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.nombre
+
+class Libro(models.Model):
+    # cod_libro = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=255)
+    año_publicacion = models.IntegerField()
+    precio = models.IntegerField()
+    imagen = models.TextField()
+    descripcion = models.CharField(max_length=2500)
+    stock = models.IntegerField()
+    firmado = models.BooleanField(default=False)
+    hay_envio_gratis = models.BooleanField(default=False)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
